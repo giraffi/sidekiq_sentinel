72f4b2620fe744fa1a7e964a80ddc1c00c34be53
sawanoboly <sawanoboriyu@higanworks.com>
2013-08-14 13:56:24 +0900

    test complete

--- lib/sidekiq_sentinel.rb
+++ lib/sidekiq_sentinel.rb
@@ -6,20 +6,15 @@ module Sidekiq
     class_eval do
       class << self
         alias :origin_client_opts :client_opts
+
         private
-        def client_opts(url, driver, timeout)
-          return origin_client_opts(url, driver, timeout) unless Sidekiq.options[:sentinel]
+        def client_opts(*args)
+          return origin_client_opts(*args) unless Sidekiq.options[:sentinel]
           require 'redis-sentinel'
-          opts = {
-            :url => url,
-            :driver => driver,
+          origin_client_opts(*args).merge({
             :master_name => Sidekiq.options[:sentinel][:master_name],
             :sentinels => Sidekiq.options[:sentinel][:sentinels]
-            }
-          if timeout
-            opts.merge!({ :timeout => timeout })
-          end
-          opts
+          })
         end
       end
     end
--- spec/sidekiq_sentinel/sidekiq_redisconnection_spec.rb
+++ spec/sidekiq_sentinel/sidekiq_redisconnection_spec.rb
@@ -1,7 +1,55 @@
 require File.expand_path('../../spec_helper', __FILE__)
 
 describe Sidekiq::RedisConnection do
-  it 'returns original client_opts' do
-    pendng
+  before do
+    @url     = 'redis://localhost:16397/5'
+    @driver  = 'SidekiqSentinel'
+    @timeout = 60
+  end
+  describe 'pass options without sentinel' do
+    it 'returns original client_opts' do
+      opts = {
+        :url => @url,
+        :driver => @driver
+      }
+      expect(Sidekiq::RedisConnection.__send__(:client_opts, @url, @driver, nil)).to eql opts
+    end
+
+    it 'returns original client_opts with timeout' do
+      opts = {
+        :url => @url,
+        :driver => @driver,
+        :timeout => @timeout
+      }
+      expect(Sidekiq::RedisConnection.__send__(:client_opts, @url, @driver, @timeout)).to eql opts
+    end
+  end
+
+  describe 'pass options with sentinel' do
+    before do
+      @sentinel = {:master_name=>"example-test", :sentinels=>[{:host=>"localhost", :port=>26379}, {:host=>"localhost", :port=>26380}]}
+      Sidekiq.options[:sentinel] = @sentinel
+    end
+
+    it 'returns option includes sentinel' do
+      opts = {
+        :url => @url,
+        :driver => @driver,
+        :master_name => @sentinel[:master_name],
+        :sentinels  => @sentinel[:sentinels]
+      }
+      expect(Sidekiq::RedisConnection.__send__(:client_opts, @url, @driver, nil)).to eql opts
+    end
+
+    it 'returns option includes sentinel with timeout' do
+      opts = {
+        :url => @url,
+        :driver => @driver,
+        :timeout => @timeout,
+        :master_name => @sentinel[:master_name],
+        :sentinels  => @sentinel[:sentinels]
+      }
+      expect(Sidekiq::RedisConnection.__send__(:client_opts, @url, @driver, @timeout)).to eql opts
+    end
   end
 end
